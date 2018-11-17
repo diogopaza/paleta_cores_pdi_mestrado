b76542dfff9074e38c2ecc34b717489df1e8ffbc
Diogo Paza <diogopazacvel@gmail.com>
2018-11-17 09:46:44 -0200

    imagem recebendo paleta

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>Trabalho prático paleta de cores Mestrado Ciências da Computação UEM</h1>
+
+<p></p>
+
--- paleta.py
+++ paleta.py
@@ -6,7 +6,7 @@ import unittest
 img = cv2.imread("militar.jpg",0)
 
 SV=180
-PALETA=250
+PALETA=200
 
 #Função para gerar uma paleta de cores com início e fim
 def criar_paleta(inicio, fim):
@@ -16,11 +16,33 @@ def criar_paleta(inicio, fim):
     h=np.linspace(inicio, fim,256)
     s=np.tile(SV,256)
     i=np.tile(SV,256)
-    print(s)
+
+    #Dando nova forma ao array
+    novoH=np.tile(h.reshape(256,1), PALETA)
+    novoS=np.tile(s.reshape(256,1), PALETA)
+    novoI=np.tile(i.reshape(256,1), PALETA)
+
+    #converte para 8bits
+    novoH=np.uint8(novoH)
+    novoS=np.uint8(novoS)
+    novoI=np.uint8(novoI)
+    minhaPaleta = np.dstack((np.dstack((novoH,novoS)), novoI))
+    return minhaPaleta
 
 #criando nova imagem com 3 canais 
 img2=np.zeros( (img.shape[0], img.shape[1],3 ))
 
+#gera paleta HSI
+paleta = criar_paleta(30,230)
+cv2.imshow('minha_paleta', paleta)
+#percorrendo cores
+cores_paleta=paleta[:,0]
+img=cores_paleta[img]
+
+bgr_img=cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+bgr_paleta=cv2.cvtColor(paleta, cv2.COLOR_HSV2BGR)
+
+#cv2.imshow('Paleta de cores',bgr_paleta )
+#cv2.imshow('imagem Colorida',bgr_img )
 
 
-criar_paleta(10,80)
